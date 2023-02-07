0ae3af606678df936f974b5fd21065a656f595a3
Batı Gökovalı <batigokovali@icloud.com>
2023-02-07 22:08:08 +0300

    Fixed the search function

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,11 +1,10 @@
 import { Row, Nav, InputGroup, Form, Col } from "react-bootstrap";
-import { Component } from "react";
 import Container from "react-bootstrap/esm/Container";
 import SingleBook from "./SingleBook";
 import { useEffect, useState } from "react";
 
 const BookList = (props) => {
-  const [searchQuery, setSearch] = useState([]);
+  const [searchQuery, setSearch] = useState("");
 
   return (
     <Container fluid className="px-0 mx-1 justify-content-center">
@@ -14,10 +13,9 @@ const BookList = (props) => {
           <Form.Control
             className="border-dark mt-3"
             placeholder="Type to Search Books..."
-            onChange={(e) => {
-              setSearch({
-                searchQuery: e.target.value,
-              });
+            value={searchQuery}
+            onChange={(event) => {
+              setSearch(event.target.value);
             }}
           ></Form.Control>
         </InputGroup>
@@ -42,48 +40,5 @@ const BookList = (props) => {
     </Container>
   );
 };
-
-// class BookList extends Component {
-//   state = {
-//     searchQuery: "",
-//   };
-
-//   render() {
-//     return (
-//       <Container fluid className="px-0 mx-1 justify-content-center">
-//         <Nav>
-//           <InputGroup className="col-12 col-lg-6 mx-auto mb-4">
-//             <Form.Control
-//               className="border-dark mt-3"
-//               placeholder="Type to Search Books..."
-//               onChange={(event) => {
-//                 this.setState({
-//                   searchQuery: event.target.value,
-//                 });
-//               }}
-//             ></Form.Control>
-//           </InputGroup>
-//         </Nav>
-//         <Row className="mx-1 justify-content-center">
-//           <Col>
-//             <Row>
-//               {this.props.FantasyBooks.map((book) => {
-//                 return (
-//                   book.title.toLowerCase().includes(this.state.searchQuery) && (
-//                     <SingleBook
-//                       setAsin={this.props.booklistAsin}
-//                       key={book.asin}
-//                       singleBook={book}
-//                     ></SingleBook>
-//                   )
-//                 );
-//               })}
-//             </Row>
-//           </Col>
-//         </Row>
-//       </Container>
-//     );
-//   }
-// }
 
 export default BookList;
